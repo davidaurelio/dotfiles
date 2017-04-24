cc40155f08cca153bd0ac9456380f429a327c7e6
David Aurelio <davidaurelio@fb.com>
2017-04-24 01:43:38 +0100

    Refine for fish and bash

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,15 @@
+set -gx HOMEBREW_NO_ANALYTICS 1
 test -f ~/.aliases; and . ~/.aliases
+
+# set env vars
+cat ~/.env ~/.env.local | while read N V
+  set -xg $N (eval echo $V)
+end
+
+# append to path
+cat ~/.paths ~/.paths.local | while read P
+  set -xg PATH $PATH P
+end
+
 test -f ~/.config/fish/config.local.fish; and . ~/.config/fish/config.local.fish
+fish_vi_mode
