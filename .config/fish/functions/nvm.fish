f30033cd08cc680e3394321139d4ad9f8e06c0d3
David Aurelio <davidaurelio@fb.com>
2017-04-26 01:13:29 +0100

    don't rely on brew or nvm being available

--- .config/fish/functions/nvm.fish
+++ .config/fish/functions/nvm.fish
@@ -1,4 +1,9 @@
 set -gx NVM_DIR ~/.nvm
 function nvm
-  bass source (brew --prefix)/opt/nvm/nvm.sh --no-use ';' nvm $argv
+  if type -p brew 2> /dev/null and [ -f (brew --prefix)/opt/nvm/nvm.sh ]
+    bass source (brew --prefix)/opt/nvm/nvm.sh --no-use ';' nvm $argv
+  else
+    echo "nvm is not installed" >&2
+    return 1
+  end
 end
