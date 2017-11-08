b5bcc7ce28e6983f89f2c49948d97bacf90dad2c
David Aurelio <dev@david-aurelio.com>
2017-11-08 22:49:31 +0000

    don't print path to homebrew when running nvm

--- .config/fish/functions/nvm.fish
+++ .config/fish/functions/nvm.fish
@@ -1,6 +1,6 @@
 set -gx NVM_DIR ~/.nvm
 function nvm
-  if type -p brew 2> /dev/null and [ -f (brew --prefix)/opt/nvm/nvm.sh ]
+  if type -p -q brew 2> /dev/null and [ -f (brew --prefix)/opt/nvm/nvm.sh ]
     bass source (brew --prefix)/opt/nvm/nvm.sh --no-use ';' nvm $argv
   else
     echo "nvm is not installed" >&2
