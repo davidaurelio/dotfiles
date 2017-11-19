2861cd3804e76286918755adc56a9138ab972bf8
David Aurelio <dev@david-aurelio.com>
2017-11-19 23:10:19 +0000

    fix opam setup

--- .opam/opam-init/variables.fish
+++ .opam/opam-init/variables.fish
@@ -1,6 +1,6 @@
-test -d "~/.opam/system/bin"; and set -gx PATH $PATH "~/.opam/system/bin";
-test -d "~/.opam/system/lib/toplevel"; and set -gx OCAML_TOPLEVEL_PATH "~/.opam/system/lib/toplevel";
-test -d "~/.opam/system/lib/perl5:$PERL5LIB"; and set -gx PERL5LIB "~/.opam/system/lib/perl5:$PERL5LIB";
-test -d "~/.opam/system/man"; and set -gx MANPATH $MANPATH "~/.opam/system/man";
+test -d ~/.opam/system/bin; and set -gx PATH ~/.opam/system/bin $PATH;
+test -d ~/.opam/system/lib/toplevel; and set -gx OCAML_TOPLEVEL_PATH ~/.opam/system/lib/toplevel;
+test -d ~/.opam/system/lib/perl5; and set -gx "PERL5LIB" ~/.opam/system/lib/perl5:"$PERL5LIB";
+test -d ~/.opam/system/man; and set -gx MANPATH $MANPATH ~/.opam/system/man;
 set -gx OPAMUTF8MSGS "1";
-test -d "~/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"; and set -gx CAML_LD_LIBRARY_PATH "~/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs";
+test -d ~/.opam/system/lib/stublibs; and set -gx CAML_LD_LIBRARY_PATH ~/.opam/system/lib/stublibs:"/usr/local/lib/ocaml/stublibs";
