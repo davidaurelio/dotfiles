2e3bbf50c9d8f76fbe88a48b8bb9f78c86d61aee
David Aurelio <158859+davidaurelio@users.noreply.github.com>
2020-04-08 10:30:15 +0200

    [Shell][Fish] Add error indicator
    For any command that fails, add a red bar after its output containing the exit code, and the command itself.
    
    Output is limited to 72 chars.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,14 @@
+function fish_prompt_postexec --on-event fish_postexec
+    set -l last_status $status
+    if [ "$last_status" != 0 ]
+        set_color --background red
+        set_color --bold white
+        printf "╰─⯈ % 3s: " $last_status
+        string replace --regex '(.{60}).{4,}' '$1...' $argv[1]
+        set_color normal
+    end
+end
+
 function fish_prompt
     set -l color_cwd
     set -l suffix
@@ -16,13 +27,13 @@ function fish_prompt
             set suffix '$'
     end
 
-    if [ "$HOME" != (eval echo "~$USER") ];
+    if [ "$HOME" != (eval echo "~$USER") ]
         set user $USER
     end
 
     set -l vcs (hg_tip "(%s)")
     if [ -z "$vcs" ]
-        set vcs (__fish_git_prompt "(%s)")
+        set vcs (fish_git_prompt "(%s)")
     end
 
     echo -n -s "$user" \
