434a5120061fc12ee22ac439b731d77faf2a2be8
David Aurelio <158859+davidaurelio@users.noreply.github.com>
2020-04-29 23:57:31 +0200

    [PowerShell][win] Move config to ~/.config/powershell
    `~/.config/powershell/` is where *pwsh* expects its configuration on macos (& linux?). mv it there, and source it from the paths that windows expects instead of using hardlinks. That might make it easier to source from the profile file.

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 ﻿function dotfiles { git --git-dir=$HOME/.dotfiles --work-tree=$HOME $args }
 
 # include local configuration
-$localConfig = "$HOME/.config\dotfiles\Documents\PowerShell\Profile.local.ps1"
+$localConfig = "$HOME/.config/powershell/Microsoft.PowerShell_profile.local.ps1"
 if (Test-Path $localConfig -PathType Leaf) {
   . $localConfig
 }
