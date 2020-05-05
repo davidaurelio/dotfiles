46cd25370f5b243f6d902378cb42cbc1e447bad7
David Aurelio <158859+davidaurelio@users.noreply.github.com>
2020-05-05 17:50:51 +0200

    [Powershell] set environment variables from .env

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,15 @@
 function dotfiles { git --git-dir=$HOME/.dotfiles --work-tree=$HOME $args }
 
 # include local configuration
-$localConfig =$PSCommandPath -replace '\.ps1', '.local$0'
-if (Test-Path $localConfig -PathType Leaf) {
-  . $localConfig
+if (Test-Path ($PSCommandPath -replace '\.ps1', '.local$0') -PathType Leaf) {
+  . ($PSCommandPath -replace '\.ps1', '.local$0')
+}
+
+foreach ($file in "$HOME/.env", "$HOME/.env.local")  {
+    if (Test-Path $file -PathType Leaf) {
+        Get-Content $file | ForEach-Object $line {
+            $var = $_ -Split '=',2
+            Invoke-Expression ('$Env:{0} = "{1}"' -f $var[0],$var[1])
+        }
+    }
 }
