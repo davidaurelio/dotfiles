7392c4525c4cb40f6aea11b26bc1cf1788d9ee71
David Aurelio <158859+davidaurelio@users.noreply.github.com>
2024-08-07 03:50:47 -0700

    [linux] fix locale fix

--- .config/fish/conf.d/01-locale-check.fish
+++ .config/fish/conf.d/01-locale-check.fish
@@ -1,3 +1,6 @@
 status --is-interactive
 and test -f /usr/bin/locale-check -a -x /usr/bin/locale-check
-and export (/usr/bin/locale-check C.UTF-8)
+and /usr/bin/locale-check C.UTF-8 | while read -d= N V
+  echo -- $V | read --tokenize --list V
+  set -xg $N $V
+end
